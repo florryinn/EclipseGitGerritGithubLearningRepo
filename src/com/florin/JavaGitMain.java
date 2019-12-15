55ace283aa247cecc1f1ef861053254771080828
Florin Olteanu <florryinn@yahoo.com>
2019-12-16 00:26:43 +0200

    Second commit for Java Git Gerrit Github Learning

--- src/com/florin/JavaGitMain.java
+++ src/com/florin/JavaGitMain.java
@@ -4,7 +4,7 @@ public class JavaGitMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("initial test");
+		System.out.println("second test");
 	}
 
 }
